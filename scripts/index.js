66967e28c778537ccbd0b4975cb8fd8cf5c3a3d0
DmnDesign <dnaumov909@gmail.com>
2023-11-12 02:06:18 +0500

    Update in my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+//Cards Array
 const initialCards = [
   (object1 = {
     name: "Yosemite Valley",
@@ -25,6 +26,10 @@ const initialCards = [
   }),
 ];
 
+/*----------------------------------------------------------------------------*/
+/*----------------------------------ELEMENTS----------------------------------*/
+/*----------------------------------------------------------------------------*/
+
 //Selecting buttons and form
 const profileEditButton = document.querySelector(".profile__edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
@@ -33,10 +38,26 @@ const profileCloseButton = profileEditModal.querySelector(
 );
 
 //Selecting title and description with form inputs
-let profileFormName = document.querySelector(".modal__form-name");
-let profileFormDescription = document.querySelector(".modal__form-description");
-let profileName = document.querySelector(".profile__title");
-let profileDescription = document.querySelector(".profile__description");
+const profileFormName = document.querySelector(".modal__form-name");
+const profileFormDescription = document.querySelector(
+  ".modal__form-description"
+);
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+//Selecting save button
+const profileSaveButton = profileEditModal.querySelector(".modal__save-button");
+
+//Selecting card list
+const cardList = document.querySelector(".cards__list");
+
+//Selecting card template
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+
+/*-----------------------------------------------------------------------------*/
+/*----------------------------------FUNCTIONS----------------------------------*/
+/*-----------------------------------------------------------------------------*/
 
 //Function which add class that change visibility of form
 function toggleForm() {
@@ -47,20 +68,43 @@ function toggleForm() {
   profileFormDescription.value = profileDescription.textContent;
 }
 
-//Two onClick events one open form and another close form
-profileEditButton.addEventListener("click", toggleForm);
-profileCloseButton.addEventListener("click", toggleForm);
-
-//Selecting save button
-const profileSaveButton = profileEditModal.querySelector(".modal__save-button");
-
-//New function which will make Save button work properly
+//Function which will make Save button work properly
 function saveFormChanges(event) {
   event.preventDefault();
   profileName.textContent = profileFormName.value;
   profileDescription.textContent = profileFormDescription.value;
   toggleForm();
 }
+
+//Function which will take all cards from array
+function getCardElement(data) {
+  //Selecting card and cloning it same amount of times as array length
+  const cardElement = cardTemplate.cloneNode(true);
+  //Selecting images and title of our card
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+  //Setting text, image and alt data same as arrays data
+  cardTitle.textContent = data.name;
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  //Adding styling to our images because it being removed because we using link as source
+  cardImage.style.borderRadius = "10px 10px 0px 0px";
+  return cardElement;
+}
+
+//Function that will make all cards visible
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardList.append(cardElement);
+});
+
+/*-----------------------------------------------------------------------------------*/
+/*----------------------------------EVENT LISTENERS----------------------------------*/
+/*-----------------------------------------------------------------------------------*/
+
+//Two onClick events one open form and another close form
+profileEditButton.addEventListener("click", toggleForm);
+profileCloseButton.addEventListener("click", toggleForm);
 
 //OnClick event which will save changes that we entered in form and close it
 profileSaveButton.addEventListener("click", saveFormChanges);
