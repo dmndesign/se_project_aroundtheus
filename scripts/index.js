9d6ffd1139e64d4f17a13303697daef0d862d118
DmnDesign <dnaumov909@gmail.com>
2023-11-12 14:46:09 +0500

    Updates in my project

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,29 @@
 //Cards Array
 const initialCards = [
-  (object1 = {
+  {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
-  }),
-  (object2 = {
+  },
+  {
     name: "Lake Louise",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
-  }),
-  (object3 = {
+  },
+  {
     name: "Bald Mountains",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
-  }),
-  (object4 = {
+  },
+  {
     name: "Latemar",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
-  }),
-  (object5 = {
+  },
+  {
     name: "Vanoise National Park",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
-  }),
-  (object6 = {
+  },
+  {
     name: "Lago di Braies",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
-  }),
+  },
 ];
 
 /*----------------------------------------------------------------------------*/
@@ -45,8 +45,8 @@ const profileFormDescription = document.querySelector(
 const profileName = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
 
-//Selecting save button
-const profileSaveButton = profileEditModal.querySelector(".modal__save-button");
+//Selecting form for submit event
+const profileEditForm = profileEditModal.querySelector(".modal__form");
 
 //Selecting card list
 const cardList = document.querySelector(".cards__list");
@@ -59,21 +59,17 @@ const cardTemplate =
 /*----------------------------------FUNCTIONS----------------------------------*/
 /*-----------------------------------------------------------------------------*/
 
-//Function which add class that change visibility of form
-function toggleForm() {
-  profileEditModal.classList.toggle("modal__opened");
-  /*Form inputs values of name and description will be same,
-  as current name and description on page when form opened*/
-  profileFormName.value = profileName.textContent;
-  profileFormDescription.value = profileDescription.textContent;
+//Function which add class that change visibility of modal
+function togglePopup() {
+  profileEditModal.classList.toggle("modal_opened");
 }
 
-//Function which will make Save button work properly
-function saveFormChanges(event) {
-  event.preventDefault();
-  profileName.textContent = profileFormName.value;
-  profileDescription.textContent = profileFormDescription.value;
-  toggleForm();
+/*Function that makes inputs values of name and description being
+same as current name and description on page when form opened*/
+function openProfilePopup() {
+  profileFormName.value = profileName.textContent;
+  profileFormDescription.value = profileDescription.textContent;
+  togglePopup();
 }
 
 //Function which will take all cards from array
@@ -87,9 +83,14 @@ function getCardElement(data) {
   cardTitle.textContent = data.name;
   cardImage.src = data.link;
   cardImage.alt = data.name;
-  //Adding styling to our images because it being removed because we using link as source
-  cardImage.style.borderRadius = "10px 10px 0px 0px";
   return cardElement;
+}
+
+function saveFormChanges(event) {
+  event.preventDefault();
+  profileName.textContent = profileFormName.value;
+  profileDescription.textContent = profileFormDescription.value;
+  togglePopup();
 }
 
 //Function that will make all cards visible
@@ -99,12 +100,16 @@ initialCards.forEach((cardData) => {
 });
 
 /*-----------------------------------------------------------------------------------*/
+/*----------------------------------SUBMIT HANDLERS----------------------------------*/
+/*-----------------------------------------------------------------------------------*/
+
+//OnClick event which will save changes that we entered in form and close it
+profileEditForm.addEventListener("submit", saveFormChanges);
+
+/*-----------------------------------------------------------------------------------*/
 /*----------------------------------EVENT LISTENERS----------------------------------*/
 /*-----------------------------------------------------------------------------------*/
 
 //Two onClick events one open form and another close form
-profileEditButton.addEventListener("click", toggleForm);
-profileCloseButton.addEventListener("click", toggleForm);
-
-//OnClick event which will save changes that we entered in form and close it
-profileSaveButton.addEventListener("click", saveFormChanges);
+profileEditButton.addEventListener("click", openProfilePopup);
+profileCloseButton.addEventListener("click", togglePopup);
