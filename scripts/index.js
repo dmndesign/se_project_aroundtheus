469f5a05265038cc0b2b7378efd498280e1ff0b3
DmnDesign <dnaumov909@gmail.com>
2023-11-22 17:34:27 +0500

    Huge changes in project

--- scripts/index.js
+++ scripts/index.js
@@ -30,34 +30,35 @@ const initialCards = [
 /*----------------------------------ELEMENTS----------------------------------*/
 /*----------------------------------------------------------------------------*/
 
-//Selecting buttons and form
+//Selecting profile elements
 const profileEditButton = document.querySelector(".profile__edit-button");
-const profileEditModal = document.querySelector("#profile-edit-modal");
-const profileCloseButton = profileEditModal.querySelector(
-  ".modal__close-button"
-);
-const newplaceAddButton = document.querySelector(".profile__add-button");
-const newplacePopup = document.querySelector("#newplace-popup");
-const newplaceClose = newplacePopup.querySelector(".newplace__close-button");
-const previewImageContainer = document.querySelector("#preview-image");
-const previewCloseButton = previewImageContainer.querySelector(
-  ".preview__close-button"
-);
-const previewImage = previewImageContainer.querySelector(".preview__image");
-const previewCaption = previewImageContainer.querySelector(".preview__caption");
-//Selecting title and description with form inputs
-const profileFormName = document.querySelector(".modal__form-name");
-const profileFormDescription = document.querySelector(
-  ".modal__form-description"
-);
+const profileAddCardButton = document.querySelector(".profile__add-button");
 const profileName = document.querySelector(".profile__title");
 const profileDescription = document.querySelector(".profile__description");
-const newplaceFormTitle = document.querySelector(".newplace__form-title");
-const newplaceFormLink = document.querySelector(".newplace__form-url");
 
-//Selecting form for submit event
-const profileEditForm = profileEditModal.querySelector(".modal__form");
-const newplaceForm = newplacePopup.querySelector(".newplace__form");
+//Selecting modal elements
+const modal = document.querySelector(".modal");
+const modalCloseButton = modal.querySelector(".modal__close-button");
+
+//Selecting modal profile elements
+const modalProfileFormName = modal.querySelector(".modal__form-name_profile");
+const modalProfileFormDescription = modal.querySelector(
+  ".modal__form-description_profile"
+);
+
+//Selecting modal newplace elements
+const modalNewPlaceFormTitle = modal.querySelector(
+  ".modal__form-title_newplace"
+);
+const modalNewPlaceFormLink = modal.querySelector(".modal__form-url_newplace");
+
+//Selecting modal form
+const modalProfileForm = modal.querySelector(".modal__form_profile");
+const modalNewPlaceForm = modal.querySelector(".modal__form_newplace");
+
+//Selecting modal image preview elements
+const modalImagePreview = modal.querySelector(".modal__image_preview");
+const modalImageCaption = modal.querySelector(".modal__caption_preview");
 
 //Selecting card list
 const cardList = document.querySelector(".cards__list");
@@ -70,73 +71,76 @@ const cardTemplate =
 /*----------------------------------FUNCTIONS----------------------------------*/
 /*-----------------------------------------------------------------------------*/
 
-//Function which add class that change visibility of modal
-function togglePopup() {
-  profileEditModal.classList.toggle("modal_opened");
+function openPopup() {
+  modal.classList.add("modal_opened");
 }
 
-//Function which will toggle add card pop up
-function toggleNewplacePopup() {
-  newplacePopup.classList.toggle("newplace_opened");
-}
-//Function which will toggle image preview
-function toggleImagePreview() {
-  previewImageContainer.classList.toggle("preview_opened");
+function closePopup() {
+  modal.classList.remove("modal_opened");
 }
 /*Function that makes inputs values of name and description being
 same as current name and description on page when form opened*/
 function openProfilePopup() {
-  profileFormName.value = profileName.textContent;
-  profileFormDescription.value = profileDescription.textContent;
-  togglePopup();
+  modalProfileFormName.value = profileName.textContent;
+  modalProfileFormDescription.value = profileDescription.textContent;
+  openPopup();
 }
 
 //Function which will get card data
 function getCardData(cardData) {
   //Selecting card and cloning it same amount of times as array length
   const cardElement = cardTemplate.cloneNode(true);
+
   //Selecting images and title of our card
   const cardImage = cardElement.querySelector(".card__image");
   const cardTitle = cardElement.querySelector(".card__title");
+
   //Selecting like button
   const cardLikeButton = cardElement.querySelector(".card__like-button");
+
   //Selecting delete button
   const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+
   //Setting text, image and alt data same as arrays data
   cardTitle.textContent = cardData.name;
   cardImage.src = cardData.link;
   cardImage.alt = cardData.name;
 
+  //OnClick event for heart icon which will change color of it
   cardLikeButton.addEventListener("click", () => {
     cardLikeButton.classList.toggle("card__like-button_black");
   });
 
+  //OnClick event for trash icon which will delete card
   cardDeleteButton.addEventListener("click", () => {
     cardElement.remove();
   });
 
+  //OnClick event which will open image preview with caption
   cardImage.addEventListener("click", () => {
-    previewImage.src = cardImage.src;
-    previewCaption.textContent = cardTitle.textContent;
-    toggleImagePreview();
+    modalImagePreview.src = cardImage.src;
+    modalImageCaption.textContent = cardTitle.alt;
+    openPopup();
   });
 
   return cardElement;
 }
 
+//Function which will render card
 function renderCard(cardData) {
   cardList.prepend(getCardData(cardData));
 }
 
-function saveFormChanges(event) {
+//Function which will save changes that we made with profile form
+function modalProfileFormChanges(event) {
   event.preventDefault();
   profileName.textContent = profileFormName.value;
   profileDescription.textContent = profileFormDescription.value;
-  togglePopup();
+  openPopup();
 }
 
-//Function which will add newplace card
-function newplaceCreate(event) {
+//Function which will add new place card
+function modalNewPlaceAddCardForm(event) {
   event.preventDefault();
   const link = newplaceFormLink.value;
   const name = newplaceFormTitle.value;
@@ -144,7 +148,7 @@ function newplaceCreate(event) {
     name: name,
     link: link,
   });
-  toggleNewplacePopup();
+  openPopup();
 }
 
 //Function that will make all cards visible
@@ -156,17 +160,21 @@ initialCards.forEach((cardData) => {
 /*----------------------------------SUBMIT HANDLERS----------------------------------*/
 /*-----------------------------------------------------------------------------------*/
 
-//OnClick event which will save changes that we entered in form and close it
-profileEditForm.addEventListener("submit", saveFormChanges);
-newplaceForm.addEventListener("submit", newplaceCreate);
+//Submit profile form
+modalProfileForm.addEventListener("submit", modalProfileFormChanges);
+
+//Submit new place form
+modalNewPlaceForm.addEventListener("submit", modalNewPlaceAddCardForm);
+
 /*-----------------------------------------------------------------------------------*/
 /*----------------------------------EVENT LISTENERS----------------------------------*/
 /*-----------------------------------------------------------------------------------*/
 
-//Click events close, open pop ups
+//Open profile edit form
 profileEditButton.addEventListener("click", openProfilePopup);
-profileCloseButton.addEventListener("click", togglePopup);
-newplaceAddButton.addEventListener("click", toggleNewplacePopup);
-newplaceClose.addEventListener("click", toggleNewplacePopup);
-//Close image preview on click
-previewCloseButton.addEventListener("click", toggleImagePreview);
+
+//Close any modal
+modalCloseButton.addEventListener("click", closePopup);
+
+//Open form which will add new place
+profileAddCardButton.addEventListener("click", openPopup);
