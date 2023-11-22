1f3893369227221798a9d21c71ec39b8cb37312b
DmnDesign <dnaumov909@gmail.com>
2023-11-22 18:04:23 +0500

    Updates

---
+++ README.md
@@ -0,0 +1,23 @@
+# Project: Around The U.S.
+
+**Project Description**
+
+Current project is "user profile with gallery". You can see in it "profile image", "profile name", "edit button", "add button" "profile description" and "card gallery".
+
+**Functionality and Technologies**
+
+For this project we used HTMl, BEM and CSS, and JavaScript as well.
+Also we used Media Queries to make our project responsive.
+CSS grid was very helpful to build up gallery in project.
+Use of JavaScript event listeners made our page intercative.
+Our gallery also builded from JavaScript code and functions.
+There is feature of adding/removing/liking our cards.
+We can look on image and it's caption by clicking on it.
+
+**Future Updates**
+
+Changes that we made will stay as is after being reloaded.
+
+**GitHub Pages Link**
+
+https://dmndesign.github.io/se_project_aroundtheus/
---
+++ scripts/index.js
@@ -0,0 +1,184 @@
+//Cards Array
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+  },
+];
+
+/*----------------------------------------------------------------------------*/
+/*----------------------------------ELEMENTS----------------------------------*/
+/*----------------------------------------------------------------------------*/
+
+//Selecting profile elements
+const profileEditButton = document.querySelector(".profile__edit-button");
+const profileAddCardButton = document.querySelector(".profile__add-button");
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+
+//Selecting modal elements
+const modal = document.querySelector(".modal");
+const modalCloseButton = document.querySelector(".modal__close-button");
+
+//Selecting modal profile elements
+const modalProfileFormName = document.querySelector(
+  ".modal__form-name_profile"
+);
+const modalProfileFormDescription = document.querySelector(
+  ".modal__form-description_profile"
+);
+
+//Selecting modal newplace elements
+const modalNewPlaceFormTitle = document.querySelector(
+  ".modal__form-title_newplace"
+);
+const modalNewPlaceFormLink = document.querySelector(
+  ".modal__form-url_newplace"
+);
+
+//Selecting modal form
+const modalProfileForm = document.querySelector(".modal__form_profile");
+const modalNewPlaceForm = document.querySelector(".modal__form_newplace");
+
+//Selecting modal image preview elements
+const modalImagePreview = document.querySelector(".modal__image_preview");
+const modalImageCaption = document.querySelector(".modal__caption_preview");
+
+//Selecting card list
+const cardList = document.querySelector(".cards__list");
+
+//Selecting card template
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+
+/*-----------------------------------------------------------------------------*/
+/*----------------------------------FUNCTIONS----------------------------------*/
+/*-----------------------------------------------------------------------------*/
+
+function openPopup() {
+  modal.classList.add("modal_opened");
+}
+
+function closePopup() {
+  modal.classList.remove("modal_opened");
+}
+/*Function that makes inputs values of name and description being
+same as current name and description on page when form opened*/
+function openProfilePopup() {
+  modalProfileFormName.value = profileName.textContent;
+  modalProfileFormDescription.value = profileDescription.textContent;
+  openPopup();
+}
+
+//Function which will get card data
+function getCardData(cardData) {
+  //Selecting card and cloning it same amount of times as array length
+  const cardElement = cardTemplate.cloneNode(true);
+
+  //Selecting images and title of our card
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardTitle = cardElement.querySelector(".card__title");
+
+  //Selecting like button
+  const cardLikeButton = cardElement.querySelector(".card__like-button");
+
+  //Selecting delete button
+  const cardDeleteButton = cardElement.querySelector(".card__delete-button");
+
+  //Setting text, image and alt data same as arrays data
+  cardTitle.textContent = cardData.name;
+  cardImage.src = cardData.link;
+  cardImage.alt = cardData.name;
+
+  //OnClick event for heart icon which will change color of it
+  cardLikeButton.addEventListener("click", () => {
+    cardLikeButton.classList.toggle("card__like-button_black");
+  });
+
+  //OnClick event for trash icon which will delete card
+  cardDeleteButton.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
+  //OnClick event which will open image preview with caption
+  cardImage.addEventListener("click", () => {
+    modalImagePreview.src = cardImage.src;
+    modalImageCaption.textContent = cardTitle.alt;
+    openPopup();
+  });
+
+  return cardElement;
+}
+
+//Function which will render card
+function renderCard(cardData) {
+  cardList.prepend(getCardData(cardData));
+}
+
+//Function which will save changes that we made with profile form
+function saveProfileFormChanges(event) {
+  event.preventDefault();
+  profileName.textContent = modalProfileFormName.value;
+  profileDescription.textContent = modalProfileFormDescription.value;
+  closePopup();
+}
+
+//Function which will add new place card
+function addNewPlaceCard(event) {
+  event.preventDefault();
+  const link = modalNewPlaceFormLink.value;
+  const name = modalNewPlaceFormTitle.value;
+  renderCard({
+    name: name,
+    link: link,
+  });
+  openPopup();
+}
+
+//Function that will make all cards visible
+initialCards.forEach((cardData) => {
+  renderCard(cardData);
+});
+
+/*-----------------------------------------------------------------------------------*/
+/*----------------------------------SUBMIT HANDLERS----------------------------------*/
+/*-----------------------------------------------------------------------------------*/
+
+//Submit profile form
+modalProfileForm.addEventListener("submit", saveProfileFormChanges);
+
+//Submit new place form
+modalNewPlaceForm.addEventListener("submit", addNewPlaceCard);
+
+/*-----------------------------------------------------------------------------------*/
+/*----------------------------------EVENT LISTENERS----------------------------------*/
+/*-----------------------------------------------------------------------------------*/
+
+//Open profile edit form
+profileEditButton.addEventListener("click", openProfilePopup);
+
+//Close any modal
+modalCloseButton.addEventListener("click", closePopup);
+
+//Open form which will add new place
+profileAddCardButton.addEventListener("click", openPopup);
